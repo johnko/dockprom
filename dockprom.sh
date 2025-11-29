0f9752ed16e3be5910be4e7cefd9ff64c416fd47
johnko <279736+johnko@users.noreply.github.com>
2025-11-29 14:37:46 -0500

    chore(alertmanager): dynamic config

--- dockprom.sh
+++ dockprom.sh
@@ -10,6 +10,11 @@ if [[ "stop" == "$ACTION" ]] || [[ "down" == "$ACTION" ]]; then
 elif [[ "destroy" == "$ACTION" ]]; then
   docker-compose down --volumes
 elif [[ "start" == "$ACTION" ]] || [[ "up" == "$ACTION" ]]; then
+  SLACK_URL=$(op read "op://Private/1pcli-dockprom-alertmanager-slackurl/slack_url")
+  SLACK_CHANNEL=$(op read "op://Private/1pcli-dockprom-alertmanager-slackurl/slack_channel")
+  export SLACK_URL
+  export SLACK_CHANNEL
+  envsubst <./alertmanager/config.yml.template >./alertmanager/config.yml
   docker-compose up -d
 elif [[ "logs" == "$ACTION" ]]; then
   docker-compose logs
